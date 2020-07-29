c57bbe80e07154ff410f5d5de847c552b6df7989
Hugo Souza <hugo.souza@ifood.com.br>
2020-07-29 20:14:49 -0300

    Add middleware validation params

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,14 +1,24 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
-
 import ProfileController from '../controllers/ProfileController';
 
 const profileRouter = Router();
 const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
-profileRouter.put('/', profileController.update);
+profileRouter.put('/',
+celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    old_password: Joi.string(),
+    password: Joi.string().invalid(Joi.ref('old_password')),
+    password_confirmation: Joi.string().valid(Joi.ref('password')),
+  },
+}),
+profileController.update);
 profileRouter.get('/', profileController.show);
 
 export default profileRouter;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,6 +5,8 @@ import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 
+import { errors } from 'celebrate';
+
 import uplodConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
@@ -15,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uplodConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response:Response, next:NextFunction) => {
   if(err instanceof AppError){
