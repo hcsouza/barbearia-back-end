1f91ffbc00becbf93cd3eba1f91755e1b494fdba
Hugo Souza <hugo.souza@ifood.com.br>
2020-11-18 11:40:04 -0300

    Add-ratelimit on requests

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,20 +1,22 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import routes from './routes';
 
 import { errors } from 'celebrate';
 
 import uplodConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
+import routes from './routes';
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
+
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uplodConfig.uploadsFolder));
@@ -22,25 +24,22 @@ app.use(routes);
 
 app.use(errors());
 
-app.use((err: Error, request: Request, response:Response, next:NextFunction) => {
-  if(err instanceof AppError){
-    return  response
-              .status(err.statusCode)
-              .json({
-                status: 'error',
-                message: err.message,
-               });
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
   }
 
   console.error(err);
-  return response
-          .status(500)
-          .json({
-            status: 'error',
-            message: 'Internal Server Error',
-          });
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
 });
 
 app.listen(3333, () => {
-    console.log("Server started on port 3333.")
+  console.log('Server started on port 3333.');
 });
